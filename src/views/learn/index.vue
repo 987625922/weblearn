e4843af6e38b31060b2a7af312176f00ac9350a6
98762 <987625922@qq.com>
2020-08-05 22:16:05 +0800

    精简项目

---
+++ src/views/learn/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="background:red;height:50px;width:150px;">
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Index',
+  data() {
+    return {
+      data: {
+        username: 'admin',
+        password: '111111'
+      },
+      loading: false,
+      passwordType: 'password',
+      redirect: undefined
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
