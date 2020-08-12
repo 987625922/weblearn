26899f903a6ae3bae797e642fd82af0f6d2b2781
wind <987625922@qq.com>
2020-08-12 18:04:26 +0800

    html dom

--- src/views/learn/js/index.vue
+++ src/views/learn/js/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h3>输出</h3>
+      <h3>- 输出</h3>
     <div>
     <span id="getElementByIdUse">getElementById()的使用</span>
     <button onclick='document.getElementById("getElementByIdUse").innerHTML="修改了这个p标签的值"'>修改id为getElementByIdUse的值</button>
@@ -9,7 +9,7 @@
     <button onclick='window.alert("弹出框")'>js弹出框</button>
     <button onclick='document.write("111")'>写入 HTML 输出</button>
   </div>
-  <h3>变量</h3>
+  <h3>- 变量</h3>
   <div>
       <button onclick='{
         var value;
@@ -17,7 +17,7 @@
     }'>
     如果变量声明时没有赋值，那么其值是 undefined。</button>
   </div>
-    <h3>数据类型</h3>
+    <h3>- 数据类型</h3>
     <div>
         <button onclick='{
             var length = 7;                             // 数字
@@ -44,10 +44,75 @@
         JavaScript的数据类型
         </button>
     </div>
-    <h3>数组</h3>
+    <h3>- 数组</h3>
     <div>
         <button onclick='var array = ["1","2","3"];
                         console.log(array[1])'>数组使用</button>
+    </div>
+    <h3>- HTML DOM</h3>
+    <div>
+      <h6>DOM把所有的html都转换为节点
+          整个文档 是一个节点
+          元素 是节点
+          元素属性 是节点
+          元素内容 是节点
+          注释 也是节点
+          如:
+          通过document.getElementById获取了id=d1的div标签对应的元素节点
+          然后通过attributes 获取了该节点对应的属性节点
+          接着通过childNodes获取了内容节点
+        </h6>
+        <button id="getElement" style="border: 1px solid blue;" onclick='
+        function p(s){
+            document.write(s)
+            document.write("<br>")
+            }
+            var id1 = document.getElementById("getElement")
+            p("文档的节点"+document)
+            p("元素"+id1)
+            p("属性节点"+id1.attributes[0])
+            p("内容节点"+id1.childNodes[0])
+            document.write("=====<br>attributes<br>")
+            var as = id1.attributes
+            document.write("button总共有"+as.length+"个属性")
+            document.write("分别为：")
+            for(i = 0;i < as.length;i++){
+              document.write("<br>")
+              document.write(as[i].nodeName)
+              document.write(":")
+              document.write(as[i].nodeValue)
+            }
+            document.write("<br>")
+            document.write("div的id属性值是："+as["id"].nodeValue)
+            document.write("=====<br>childNodes<br>")
+            var content = id1.childNodes[0]
+            document.write("button的内容节点名是："+content.nodeName)
+            document.write("<br>")
+            document.write("div的内容节点值是："+content.nodeValue)
+            id1.innerHTML="通过innerHTML改变内容"
+            '>hello HTML DOM</button>
+            <h6>元素上的属性，比如id,value 可以通过 . 直接访问,如果是自定义属性,可以通过如下test标签的方式获取</h6>
+            <input id="input1" test="t1" value="这是一个输入框" type="text">
+            <button onclick='var input1 = document.getElementById("input1")
+                            var s = "id = "+input1.id+"<br>"
+                            s+="class="+input1.value+"<br>"
+                            s+="test="+input1.getAttribute("test")+"<br>"
+                            s+="test"+input1.attributes["test"].nodeValue+"<br>"
+                            document.getElementById("getElement").innerHTML=s'>
+                            通过getElementById获取input元素
+                            </button>
+            <h6>一个元素节点的style属性即对应的css(通过js修改元素的css属性)</h6>
+            <button onclick='
+              document.getElementById("getElement").style.display="none"
+            '>
+            隐藏id为getElement的元素
+            </button>
+            <button onclick='
+              var id = document.getElementById("getElement")
+              id.style.display="block"
+              id.style.backgroundColor="red"
+            '>显示id为getElement的元素并改变背景颜色</button>
+            <h6>HTML DOM的事件</h6>
     </div>
   </div>
 </template>
